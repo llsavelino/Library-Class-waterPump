c62d0cdb87706be0e59a079384b8261dc9d2560b
Lenilson A.S <savelino852@gmail.com>
2025-05-20 08:34:53 -0300

    Add files via upload

---
+++ Class_pump.cpp
@@ -0,0 +1,81 @@
+namespace cpump_Lib {
+  
+    class __Base__ {
+    	public: private: friend class Bombs; protected:
+        struct PumpData { uint8_t pin; bool state; } _pump_;
+        
+        __Base__(uint8_t pin, bool state): _pump_{pin, state}
+        {
+            pinMode(_pump_.pin, OUTPUT);
+            digitalWrite(_pump_.pin, _pump_.state);
+        }
+
+        virtual ~__Base__(void) 
+        {
+            pinMode(_pump_.pin, INPUT);
+          	digitalWrite(_pump_.pin, LOW);
+        }
+      
+        virtual void swapState(bool toChange = 0) = 0;
+        virtual void analogPower(uint8_t value) = 0;
+        bool getState() const { return (*this)._pump_.state; }
+    };
+  
+  	inline namespace ScopeInternal {
+
+    	class Bombs final: public __Base__ {
+    	private:
+        	uint8_t power;
+    	public: using __Base__::getState;
+          friend class __Base__;
+        	explicit 
+          		Bombs(uint8_t pin, bool state, uint8_t powerLevel = 0)
+              	: __Base__(pin, state), power(powerLevel) {}
+
+        	void swapState(bool toChange = 0) override {
+            	(*this)._pump_.state ^= !toChange;
+            	digitalWrite(_pump_.pin, _pump_.state);
+        	}
+
+        	void analogPower(uint8_t value) override {
+            	power = (value & ~(0b00000000));
+            	analogWrite(_pump_.pin, power);
+        	}
+
+        	Bombs& operator+=(uint8_t &&value) {
+           		power = (power + (value & 0xFF)) & 0xFF;
+           	 	analogWrite((*this)._pump_.pin, this->power);
+            	return *this;
+        	}
+
+        	Bombs& operator-=(uint8_t &&value) {
+            	power = (power - (value & 0xFF)) & 0xFF;
+            	analogWrite((*this)._pump_.pin, this->power);
+            	return *(this);
+        	}
+
+        	~Bombs(void) = default;
+    	};
+    };
+}; // namespace cpump_Lib
+using jobBomb = cpump_Lib::Bombs;
+
+inline auto nxor = [](int x, int y) -> decltype(~(x ^ y)& ((!0) >> 0))
+{ return ~(x ^ y)& ((!0) >> 0); };
+
+decltype(auto) obj{ new jobBomb(0x03, (nxor(0, 1) & !0)) };
+
+		void setup(void) noexcept(0)
+		   
+          		 {return;};
+
+		void loop(void) noexcept(0)
+{
+    (*obj += (uint8_t)('@'|'$'));
+  	delay(0xFAu << 02 >> !0);
+  	(*obj -= (uint8_t)('@'|'$'));
+  	delay(0xFA << 02 >> !0);
+    obj->swapState();
+    delay(0xFAu << 02 >> !0);
+          
+};
